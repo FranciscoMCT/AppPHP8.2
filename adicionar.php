37f15ba46af88b0bbe47f133880623f2d155dc69
FranciscoMCT <51161463+FranciscoMCT@users.noreply.github.com>
2024-10-01 08:48:42 -0300

    Update adicionar.php

--- adicionar.php
+++ adicionar.php
@@ -1,4 +1,20 @@
 <?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:sqlserverlkygplon7tm2i.database.windows.net,1433; Database = Loja", "azureuser", "P@$$W)RD1234");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "azureuser", "pwd" => "{your_password_here}", "Database" => "Loja", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:sqlserverlkygplon7tm2i.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nomeProduto = $_POST['nome'];
     $precoProduto = $_POST['preco'];
