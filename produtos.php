55a97bdaa195ea940afeef0621d7eb73abdd9855
FranciscoMCT <51161463+FranciscoMCT@users.noreply.github.com>
2024-10-01 08:49:24 -0300

    Update produtos.php

--- produtos.php
+++ produtos.php
@@ -1,5 +1,18 @@
 <?php
-// Código de conexão aqui...
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:sqlserverlkygplon7tm2i.database.windows.net,1433; Database = Loja", "azureuser", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "azureuser", "pwd" => "{your_password_here}", "Database" => "Loja", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:sqlserverlkygplon7tm2i.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 
 $sql = "SELECT * FROM Produtos";
 $stmt = $conn->query($sql);
