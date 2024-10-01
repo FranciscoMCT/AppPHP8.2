84e719d3747d49016a26fc9a29fab5443d64d581
FranciscoMCT <51161463+FranciscoMCT@users.noreply.github.com>
2024-10-01 08:47:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:sqlserverlkygplon7tm2i.database.windows.net,1433; Database = Loja", "azureuser", "P@$$W)RD1234");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "azureuser", "pwd" => "{your_password_here}", "Database" => "Loja", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:sqlserverlkygplon7tm2i.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
