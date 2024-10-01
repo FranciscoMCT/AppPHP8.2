5a739cc3be91441b0ca07105a6eec8de59973caf
FranciscoMCT <51161463+FranciscoMCT@users.noreply.github.com>
2024-10-01 08:36:00 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<?php
-$serverName = "seu_ip_publico"; // IP da sua VM
-$database = "Loja";
-$username = "seu_usuario";
-$password = "sua_senha";
-
-try {
-    $conn = new PDO("sqlsrv:server=$serverName;database=$database", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Erro na conexão: " . $e->getMessage();
-}
-?>
-    
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
