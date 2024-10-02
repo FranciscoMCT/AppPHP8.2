1c0194c7eddbcc3052430e7a8e1a085888b193a3
FranciscoMCT <51161463+FranciscoMCT@users.noreply.github.com>
2024-10-02 12:19:30 -0300

    Update index.php

--- index.php
+++ index.php
@@ -25,6 +25,7 @@ try {
         <a href="index.php">Home</a>
         <a href="produtos.php">Produtos</a>
         <a href="adicionar.php">Adicionar Produto</a>
+        <a href="excluir.php">Excluir Produto></a>>
     </nav>
     <h2>Produtos Disponíveis</h2>
     <ul>
