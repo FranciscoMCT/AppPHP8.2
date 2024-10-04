29a0a2a5ca6bb55684c59cd13bc313a30f793112
FranciscoMCT <51161463+FranciscoMCT@users.noreply.github.com>
2024-10-04 17:12:25 -0300

    Update adicionar.php

--- adicionar.php
+++ adicionar.php
@@ -1,8 +1,8 @@
 <?php
---$serverName = "tcp:sqlserver63ujiagifce6s.database.windows.net,1433"; // Substitua pelo seu servidor
---$database = "loja"; // Substitua pelo seu banco de dados
---$username = "sql"; // Substitua pelo seu usuário
---$password = "xxxxxxxxx"; // Substitua pela sua senha
+$serverName = "tcp:sqlserver63ujiagifce6s.database.windows.net,1433"; // Substitua pelo seu servidor
+$database = "loja"; // Substitua pelo seu banco de dados
+$username = "sql"; // Substitua pelo seu usuário
+$password = "xxxxxxxxx"; // Substitua pela sua senha
 
 try {
     $conn = new PDO("sqlsrv:server=$serverName;database=$database", $user, $password);
