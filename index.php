e4ebf0ebef5cf491aba9c2492fcdfb670cbf37f8
FranciscoMCT <51161463+FranciscoMCT@users.noreply.github.com>
2024-10-07 16:15:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
-$serverName = "tcp:sqlserver63ujiagifce6s.database.windows.net,1433"; // Substitua pelo seu servidor
-$database = "loja"; // Substitua pelo seu banco de dados
-$user = "sql"; // Substitua pelo seu usuário
-$password = "xxxxx"; // Substitua pela sua senha
-
-try {
-    $conn = new PDO("sqlsrv:server=$serverName;database=$database", $user, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Erro na conexão: " . $e->getMessage();
-}
+require 'conexao.php';
 ?>
 
 <!DOCTYPE html>
@@ -25,6 +15,7 @@ try {
         <a href="index.php">Home</a>
         <a href="produtos.php">Produtos</a>
         <a href="adicionar.php">Adicionar Produto</a>
+        <a href="excluir.php">Excluir Produto></a>>
     </nav>
     <h2>Produtos Disponíveis</h2>
     <ul>
@@ -40,4 +31,3 @@ try {
     </ul>
 </body>
 </html>
-
