aa9cf9da823ee3cce4b5945cb165d9afdfb772b0
FranciscoMCT <51161463+FranciscoMCT@users.noreply.github.com>
2024-10-07 16:15:39 -0300

    Update adicionar.php

--- adicionar.php
+++ adicionar.php
@@ -1,15 +1,5 @@
 <?php
-$serverName = "tcp:sqlserver63ujiagifce6s.database.windows.net,1433"; // Substitua pelo seu servidor
-$database = "loja"; // Substitua pelo seu banco de dados
-$username = "sql"; // Substitua pelo seu usuário
-$password = "xxxxxxxxx"; // Substitua pela sua senha
-
-try {
-    $conn = new PDO("sqlsrv:server=$serverName;database=$database", $user, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Erro na conexão: " . $e->getMessage();
-}
+require 'conexao.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nomeProduto = $_POST['nome'];
@@ -38,6 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <a href="index.php">Home</a>
         <a href="produtos.php">Produtos</a>
         <a href="adicionar.php">Adicionar Produto</a>
+        <a href="excluir.php">Excluir Produto></a>
     </nav>
     <form method="POST">
         <label for="nome">Nome:</label>
